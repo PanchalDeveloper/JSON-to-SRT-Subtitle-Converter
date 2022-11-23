24570964c678eb43c66bb89bb28786a9bed9df31
Aman Panchal <amanpanchal509@gmail.com>
2022-11-23 12:01:01 +0530

    Fixed DarkMode bug

--- assets/js/docStyling.js
+++ assets/js/docStyling.js
@@ -3,6 +3,43 @@ window.addEventListener('scroll', checkHeaderPosition);
 function checkHeaderPosition() {
     const header = document.getElementById('navHeader');
     if (window.scrollY > 0) {
-        if (!header.className.includes('header-minimizer')) {header.classList.add('header-minimizer'); }
+        if (!header.className.includes('header-minimizer')) { header.classList.add('header-minimizer'); }
     } else { header.classList.remove('header-minimizer'); }
 }
+
+// Automatically Check & Set Color-Schemes 
+
+const checkDarkMode = () => { return (localStorage.getItem("dark-mode") || sessionStorage.getItem("os-has-dark-mode") || 'false') == 'true'; }
+
+let darkModeState = checkDarkMode();
+const useDark = window.matchMedia('(prefers-color-scheme: dark)');
+setDarkModeSessionStorage(useDark.matches);
+
+function toggleDarkMode(state) {
+    try {
+        themeTogglerBtn.checked = state;
+    } catch {
+        // console.log('Page is loading currently');
+    }
+    finally {
+        document.documentElement.classList.toggle("dark-mode", state);
+        darkModeState = state;
+    }
+}
+
+function setDarkModeLocalStorage(state) {
+    localStorage.setItem("dark-mode", state);
+}
+function setDarkModeSessionStorage(state) {
+    sessionStorage.setItem("os-has-dark-mode", state);
+}
+
+function changeThemeMode() {
+    darkModeState = !darkModeState;
+    toggleDarkMode(darkModeState);
+    setDarkModeLocalStorage(darkModeState);
+}
+
+(localStorage.getItem("dark-mode")) ? (toggleDarkMode(localStorage.getItem("dark-mode") == "true")) : (toggleDarkMode(useDark.matches));
+
+useDark.addListener((event) => { setDarkModeSessionStorage(event.matches); if(localStorage.getItem("dark-mode") != 'false'){toggleDarkMode(checkDarkMode())}; });
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,8 @@ let Subtitles = new Array;
 
 window.onload = initializer;
 
-// Automatically/Manually Check & Set Theme 
+// Manually Check & Set Color-Schemes 
+const themeTogglerBtn = document.getElementById('themeTogglerBtn');
+themeTogglerBtn.addEventListener('change', changeThemeMode);
 
-let darkModeState = false;
-
-const themeToggleBtn = document.getElementById('darkModeBtn'),
-useDark = window.matchMedia('(prefers-color-scheme: dark)');
-console.log(useDark);
-function toggleDarkMode(state) {
-    document.documentElement.classList.toggle("dark-mode", state);
-    darkModeState = state;
-}
-
-function setDarkModeLocalStorage(state) {
-    localStorage.setItem("dark-mode", state);
-}
-
-function changeThemeMode(){
-    darkModeState = !darkModeState;
-    toggleDarkMode(darkModeState);
-    setDarkModeLocalStorage(darkModeState);
-}
-
-(localStorage.getItem("dark-mode"))?(toggleDarkMode(localStorage.getItem("dark-mode") == "true")):(toggleDarkMode(useDark.matches));
-
-useDark.addListener((event)=>{toggleDarkMode(event.matches)});
-themeToggleBtn.addEventListener('click', changeThemeMode);
+themeTogglerBtn.checked = checkDarkMode();
